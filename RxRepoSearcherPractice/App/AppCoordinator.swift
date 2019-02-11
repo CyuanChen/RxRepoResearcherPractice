18936403f39f9c59eaa903b0433b3cf311f09f37
PeterChen <peterchen302@gmail.com>
2019-02-11 17:58:22 +0800

    first commit

---
+++ RxRepoSearcherPractice/App/AppCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  AppCoordinator.swift
+//  RxRepoSearcherPractice
+//
+//  Created by PeterChen on 2019/2/11.
+//  Copyright © 2019 PeterChen. All rights reserved.
+//
+
+import Foundation
---
+++ RxRepoSearcherPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxRepoSearcherPractice
+//
+//  Created by PeterChen on 2019/2/11.
+//  Copyright © 2019 PeterChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
